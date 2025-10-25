5434c7468faa9ad0c0c5b8e19443fb530b5b1036
TalhaShaikh150 <tssmart233@gmail.com>
2025-10-26 03:32:00 +0500

    Created Simple Steps to Get Started Section

--- second/tailwind-config.js
+++ second/tailwind-config.js
@@ -2,13 +2,14 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        "dark-green": "#0a2a1a",
-        "light-green": "#1b7551",
-        cream: "#fbf9f6",
-        "accent-orange": "#d69e2e",
-        "darker-green": "#2d523b",
+      "primary-dark": "#0a2a1a",
+                        "primary-light": "#1b7551",
+                        "background-cream": "#fbf9f6",
+                        "accent-orange": "#d69e2e",
+                        "darker-green": "#2d523b",
       },
     },
   },
 };
 lucide.createIcons();
+ 
