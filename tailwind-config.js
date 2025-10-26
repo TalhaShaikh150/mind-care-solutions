8935ff5a21e37ff2271a47342dc9fedec21c6a9b
TalhaShaikh150 <tssmart233@gmail.com>
2025-10-26 22:14:01 +0500

    Removed All Unnecessary Files And Redesign The Whole Navbar with Responsiveness

--- tailwind-config.js
+++ tailwind-config.js
@@ -2,9 +2,9 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        "dark-green": "#3b684c",
-        "light-green": "#90c65d",
-        cream: "#f9f8f4",
+        "primary-dark": "#0a2a1a",
+        "primary-light": "#1b7551",
+        "background-cream": "#fbf9f6",
         "accent-orange": "#d69e2e",
         "darker-green": "#2d523b",
       },
